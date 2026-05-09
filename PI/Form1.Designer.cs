22b39c4136a4cff79e4640d3c5db584a62870357
Kaio <kaiomascarenhas241@gmail.com>
2026-05-08 22:21:44 -0300

    Atualizando o btnConsultarLista, btnConsultar, btnLimpar, btnSalvar

--- PI/Form1.Designer.cs
+++ PI/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
-            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
             this.lbPlaca = new System.Windows.Forms.Label();
             this.dgvCarros = new System.Windows.Forms.DataGridView();
             this.lbModelo = new System.Windows.Forms.Label();
@@ -43,17 +43,25 @@
             this.txtMarca = new System.Windows.Forms.TextBox();
             this.txtCor = new System.Windows.Forms.TextBox();
             this.lbQuilometragem = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.nudQuilometragem = new System.Windows.Forms.NumericUpDown();
+            this.dtpAno = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConsultarLista = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarros)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuilometragem)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSalvar
             // 
             this.btnSalvar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalvar.Location = new System.Drawing.Point(12, 401);
+            this.btnSalvar.Location = new System.Drawing.Point(12, 358);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(106, 37);
+            this.btnSalvar.Size = new System.Drawing.Size(112, 37);
             this.btnSalvar.TabIndex = 0;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
@@ -62,22 +70,23 @@
             // btnEditar
             // 
             this.btnEditar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditar.Location = new System.Drawing.Point(124, 401);
+            this.btnEditar.Location = new System.Drawing.Point(12, 401);
             this.btnEditar.Name = "btnEditar";
             this.btnEditar.Size = new System.Drawing.Size(112, 37);
             this.btnEditar.TabIndex = 1;
             this.btnEditar.Text = "Editar";
             this.btnEditar.UseVisualStyleBackColor = true;
             // 
-            // btnExcluir
+            // btnConsultar
             // 
-            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExcluir.Location = new System.Drawing.Point(242, 401);
-            this.btnExcluir.Name = "btnExcluir";
-            this.btnExcluir.Size = new System.Drawing.Size(109, 37);
-            this.btnExcluir.TabIndex = 2;
-            this.btnExcluir.Text = "Consultar";
-            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(134, 401);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(109, 37);
+            this.btnConsultar.TabIndex = 2;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // lbPlaca
             // 
@@ -130,12 +139,13 @@
             // btnLimpar
             // 
             this.btnLimpar.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLimpar.Location = new System.Drawing.Point(358, 401);
+            this.btnLimpar.Location = new System.Drawing.Point(134, 358);
             this.btnLimpar.Name = "btnLimpar";
-            this.btnLimpar.Size = new System.Drawing.Size(113, 37);
+            this.btnLimpar.Size = new System.Drawing.Size(109, 37);
             this.btnLimpar.TabIndex = 8;
             this.btnLimpar.Text = "Limpar";
             this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // lbCor
             // 
@@ -180,25 +190,105 @@
             // 
             this.lbQuilometragem.AutoSize = true;
             this.lbQuilometragem.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbQuilometragem.Location = new System.Drawing.Point(13, 208);
+            this.lbQuilometragem.Location = new System.Drawing.Point(13, 192);
             this.lbQuilometragem.Name = "lbQuilometragem";
             this.lbQuilometragem.Size = new System.Drawing.Size(123, 19);
             this.lbQuilometragem.TabIndex = 15;
             this.lbQuilometragem.Text = "Quilometragem:";
             // 
-            // numericUpDown1
+            // nudQuilometragem
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(85, 121);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(151, 20);
-            this.numericUpDown1.TabIndex = 16;
+            this.nudQuilometragem.Location = new System.Drawing.Point(143, 191);
+            this.nudQuilometragem.Name = "nudQuilometragem";
+            this.nudQuilometragem.Size = new System.Drawing.Size(208, 20);
+            this.nudQuilometragem.TabIndex = 17;
+            // 
+            // dtpAno
+            // 
+            this.dtpAno.Location = new System.Drawing.Point(85, 121);
+            this.dtpAno.Name = "dtpAno";
+            this.dtpAno.Size = new System.Drawing.Size(211, 20);
+            this.dtpAno.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(488, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(488, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(488, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(488, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "label6";
+            // 
+            // btnConsultarLista
+            // 
+            this.btnConsultarLista.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultarLista.Location = new System.Drawing.Point(250, 358);
+            this.btnConsultarLista.Name = "btnConsultarLista";
+            this.btnConsultarLista.Size = new System.Drawing.Size(235, 80);
+            this.btnConsultarLista.TabIndex = 25;
+            this.btnConsultarLista.Text = "Consultar Lista de Carros";
+            this.btnConsultarLista.UseVisualStyleBackColor = true;
+            this.btnConsultarLista.Click += new System.EventHandler(this.btnConsultarLista_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(936, 450);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnConsultarLista);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpAno);
+            this.Controls.Add(this.nudQuilometragem);
             this.Controls.Add(this.lbQuilometragem);
             this.Controls.Add(this.txtCor);
             this.Controls.Add(this.txtMarca);
@@ -211,13 +301,13 @@
             this.Controls.Add(this.lbModelo);
             this.Controls.Add(this.dgvCarros);
             this.Controls.Add(this.lbPlaca);
-            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnConsultar);
             this.Controls.Add(this.btnEditar);
             this.Controls.Add(this.btnSalvar);
             this.Name = "Form1";
             this.Text = "Tela do projeto integrador sobre carros";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarros)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuilometragem)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,7 +317,7 @@
 
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Button btnEditar;
-        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Label lbPlaca;
         private System.Windows.Forms.DataGridView dgvCarros;
         private System.Windows.Forms.Label lbModelo;
@@ -240,7 +330,15 @@
         private System.Windows.Forms.TextBox txtMarca;
         private System.Windows.Forms.TextBox txtCor;
         private System.Windows.Forms.Label lbQuilometragem;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown nudQuilometragem;
+        private System.Windows.Forms.DateTimePicker dtpAno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnConsultarLista;
     }
 }
 
--- PI/Form1.cs
+++ PI/Form1.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySqlConnector;
 
 namespace PI
 {
@@ -18,23 +19,37 @@ namespace PI
             InitializeComponent();
         }
 
+        string DADOS_CONEXAO =
+                "server=localhost;user=root;password=;database=db_carros_projeto_integrador";
+
         private void btnSalvar_Click(object sender, EventArgs e)
         {
             string campoPlaca = txtPlaca.Text;
+            string campoModelo = txtModelo.Text;
+            string campoMarca = txtMarca.Text;
+            string campoData = dtpAno.Text;
+            string campoCor = txtCor.Text;
+            string campoQuilometragem = nudQuilometragem.Value.ToString();
+
+            DateTime dataConvertida = DateTime.Parse(campoData); 
+
             int controleLinhasAfetadas = 0;
 
-            string dadosConexao = 
-                "server=localhost;user=root;password=;database=db_carros_projeto_integrador";
-            using (MySqlConnection conn = new MySqlConnection(dadosConexao) )
+            
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO) )
             {// utilizo das informações
                 conn.Open();
-                string scriptInsert = "INSERT INTO tb_carros (placa) VALUE (@placa)"; 
+                string scriptInsert = "INSERT INTO tb_carros (placa, modelo, marca, ano, cor, quilometragem) " +
+                      "VALUES (@placa, @modelo, @marca, @ano, @cor, @km)";
 
                 using (MySqlCommand comando = new MySqlCommand(scriptInsert,conn))
                 {
                     comando.Parameters.AddWithValue("@placa", campoPlaca);
-
-                    controleLinhasAfetadas = comando.ExecuteNonQuery();
+                    comando.Parameters.AddWithValue("@modelo", campoModelo);
+                    comando.Parameters.AddWithValue("@marca", campoMarca);
+                    comando.Parameters.AddWithValue("@ano", campoData); // Se for int, o banco aceita direto
+                    comando.Parameters.AddWithValue("@cor", campoCor);
+                    comando.Parameters.AddWithValue("@km", campoQuilometragem); // Passando o decimal do NumericUpDown
                 }
                 conn.Close();
             }//MysqlConnection
@@ -46,9 +61,93 @@ namespace PI
             }
         }
 
-        private void txtPlaca_TextChanged(object sender, EventArgs e)
+
+        private void btnLimpar_Click(object sender, EventArgs e)
         {
 
+            txtPlaca.Clear();
+            txtModelo.Clear();
+            txtMarca.Clear();
+            txtCor.Clear();
+
+            dtpAno.Value = DateTime.Now;
+
+            nudQuilometragem.Value = 0;
+
+            txtPlaca.Focus();
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(DADOS_CONEXAO))
+            {
+
+                conn.Open();
+                string idConsulta = txtPlaca.Text;
+                string scriptConsultaIndividual = "SELECT * FROM tb_carros WHERE = @id_carro";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptConsultaIndividual, conn))
+                {
+                    comando.Parameters.AddWithValue("@id_carro", idConsulta);
+
+                    var dadosResultado = comando.ExecuteReader();
+
+                    //while (dadosResultado.Read())
+                    //{
+                    //    lbIdResultado.Text = dadosResultado["id"].ToString();
+                    //    lbIdResultado.Text = dadosResultado["nome"].ToString();
+                    //    lbIdResultado.Text = dadosResultado["serviço"].ToString();
+                    //    lbIdResultado.Text = dadosResultado["data_serviço"].ToString();
+                    //}
+
+                }
+
+                conn.Close();
+            }
+        }
+
+
+        private void btnConsultarLista_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection())
+            {
+                conn.Open ();
+                //string campoServico = cbConsulta.Text;
+                string scriptConsulta = "";
+
+                //if (campoServico != "")
+                //{
+                //    scriptConsulta = "SELECT * FROM tb_carros WHERE servico = @servico";
+                //}
+                //else
+                //{
+                //    scriptConsulta = "SELECT * FROM tb_carros";
+                //}
+
+
+                //string scriptConsulta = "SELECT * FROM tb_carros";
+
+                using (MySqlCommand comando = new MySqlCommand (scriptConsulta, conn))
+                {
+                    MySqlDataAdapter resultadoConsultaMySql = new MySqlDataAdapter(comando);
+
+                    DataTable dt = new DataTable();
+
+                    resultadoConsultaMySql.Fill(dt);
+
+                    dgvCarros.DataSource = dt;
+
+                }
+
+                conn.Close();
+            }
+        }
+
+
+        private void txtPlaca_TextChanged(object sender, EventArgs e)
+        {
+            
         }
 
     }
